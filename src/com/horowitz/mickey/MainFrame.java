939edb4a5adee3b998b6f57bc933903d71db58f6
zhristov <zhristov@gmail.com>
2014-09-16 12:53:15 +0300

    618 fixed isRunning method

--- src/com/horowitz/mickey/MainFrame.java
+++ src/com/horowitz/mickey/MainFrame.java
@@ -57,7 +57,7 @@ public final class MainFrame extends JFrame {
 
   private final static Logger LOGGER        = Logger.getLogger(MainFrame.class.getName());
 
-  private static final String APP_TITLE     = "Mickey v0.617";
+  private static final String APP_TITLE     = "Mickey v0.618";
 
   private boolean             _refresh      = true;
   private boolean             _devMode      = false;
@@ -1220,7 +1220,7 @@ public final class MainFrame extends JFrame {
     Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
     for (Iterator<Thread> it = threadSet.iterator(); it.hasNext();) {
       Thread thread = it.next();
-      if (thread.getName().equals("LOADTRAINS")) {
+      if (thread.getName().equals(threadName)) {
         isRunning = true;
         break;
       }
