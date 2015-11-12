b3dd008bddae1612fc301164fbe8b63172fe9930
Jeff Horowitz <horowitz2009@users.noreply.github.com>
2015-11-12 15:05:39 +0200

    DropboxRemote improved
    Now it accepts pomg command. Also it has ping toggle button

--- src/com/horowitz/mickey/DropboxRemote.java
+++ src/com/horowitz/mickey/DropboxRemote.java
@@ -9,23 +9,22 @@ import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JToggleButton;
+import javax.swing.JToolBar;
 
 import com.horowitz.mickey.common.MyImageIO;
 import com.horowitz.mickey.service.Service;
 
-public class DropboxRemote {
+public class DropboxRemote extends JFrame {
   public static void main(String[] args) {
-    JFrame frame = new JFrame("DropboxRemote 926");
-    frame.setSize(350, 150);
-    outputConsole = new JTextArea(8, 14);
-    frame.getContentPane().add(new JScrollPane(outputConsole), BorderLayout.CENTER);
-    frame.setAlwaysOnTop(true);
-    frame.setVisible(true);
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     DropboxRemote dr = new DropboxRemote();
     dr.runListener();
   }
@@ -34,9 +33,25 @@ public class DropboxRemote {
   private MouseRobot       _mouse;
   private Service          service = new Service("-tiktak");
   private static JTextArea outputConsole;
+  private JToggleButton    _pingToggle;
 
   public DropboxRemote() {
     super();
+    setTitle("DropboxRemote 928");
+    setSize(350, 150);
+    outputConsole = new JTextArea(8, 14);
+    JToolBar toolbar = new JToolBar();
+    _pingToggle = new JToggleButton("Ping");
+    toolbar.add(_pingToggle);
+    toolbar.setFloatable(false);
+
+    getContentPane().setLayout(new BorderLayout());
+    getContentPane().add(toolbar, BorderLayout.NORTH);
+    getContentPane().add(new JScrollPane(outputConsole), BorderLayout.CENTER);
+    setAlwaysOnTop(true);
+    setVisible(true);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     try {
       _mouse = new MouseRobot();
 
@@ -48,7 +63,7 @@ public class DropboxRemote {
     }
   }
 
-  private void processRequests() {
+  private void processRequests() throws RobotInterruptedException {
     publish("" + ++turn);
     String[] requests = service.getActiveRequests();
     for (String r : requests) {
@@ -58,8 +73,9 @@ public class DropboxRemote {
 
         service.inProgress(r);
         processClick(r);
-      } else {
-        // publish("passing this " + r);
+      } else if (r.startsWith("p")) {
+        captureScreen();
+        _mouse.delay(100, false);
       }
     }
 
@@ -89,11 +105,44 @@ public class DropboxRemote {
       service.done(r);
     }
   }
+  
+  private void deleteOlder(String prefix, int amountFiles) {
+    File f = new File(".");
+    File[] files = f.listFiles();
+    List<File> targetFiles = new ArrayList<File>(6);
+    int cnt = 0;
+    for (File file : files) {
+      if (!file.isDirectory() && file.getName().startsWith(prefix)) {
+        targetFiles.add(file);
+        cnt++;
+      }
+    }
+
+    if (cnt > amountFiles) {
+      // delete some files
+      Collections.sort(targetFiles, new Comparator<File>() {
+        public int compare(File o1, File o2) {
+          if (o1.lastModified() > o2.lastModified())
+            return 1;
+          else if (o1.lastModified() < o2.lastModified())
+            return -1;
+          return 0;
+        };
+      });
+
+      int c = cnt - 5;
+      for (int i = 0; i < c; i++) {
+        File fd = targetFiles.get(i);
+        fd.delete();
+      }
+    }
+  }
 
   private void captureScreen() {
     final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     writeImage(new Rectangle(0, 0, screenSize.width, screenSize.height), "ping " + DateUtils.formatDateForFile(System.currentTimeMillis()) + ".png");
-
+    publish("ping");
+    deleteOlder("ping", 12);
   }
 
   public void writeImage(Rectangle rect, String filename) {
@@ -124,8 +173,17 @@ public class DropboxRemote {
     Thread settingsThread = new Thread(new Runnable() {
       public void run() {
         boolean stop = false;
+        long start = System.currentTimeMillis();
         do {
           try {
+            long now = System.currentTimeMillis();
+            if (_pingToggle.isSelected()) {
+              if (now - start > 1 * 60 * 1000) {
+                start = now;
+                captureScreen();
+              }
+            
+            }
             processRequests();
           } catch (Throwable t) {
             publish("-= ERROR =-");
