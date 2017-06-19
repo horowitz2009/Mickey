d45cbcc79070c9f125a2944de2da9c5bab8fb0e0
Jeff Horowitz <horowitz2009@gmail.com>
2017-06-19 20:55:55 +0300

    1.5 wagr changed font

--- src/com/horowitz/mickey/MainFrame.java
+++ src/com/horowitz/mickey/MainFrame.java
@@ -91,7 +91,7 @@ public final class MainFrame extends JFrame {
 
   private final static Logger   LOGGER              = Logger.getLogger(MainFrame.class.getName());
 
-  private static final String   APP_TITLE           = "v1.4";
+  private static final String   APP_TITLE           = "v1.5";
 
   private boolean               _devMode            = false;
 
